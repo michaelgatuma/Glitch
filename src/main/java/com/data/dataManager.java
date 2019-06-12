f697888b0420b5a72bb2ca7dc4cd37b5d82ddc20
Michael <mgate4410@mail.com>
2019-06-12 05:03:37 +0300

    Remove code that handles Hypersonic2 database connection
    -Remove code class that handles the JDBC Connection.
    -Add code to test if DB file exists before creating SQLite connection,
    or else prepend 'src/' to database path for development purpose.

--- src/main/java/com/data/dataManager.java
+++ src/main/java/com/data/dataManager.java
@@ -1,5 +1,6 @@
 package com.data;
 
+import java.io.File;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -8,11 +9,19 @@ import java.sql.SQLException;
  * @author Michael
  */
 public class dataManager {
+    private static String path = "main/java/com/data/local.db";
 
     public static Connection getSQLiteConnection() {
+        //run below block if database path does not exist
+        if (!new File(path).exists()) {
+            //For Development purposes this block will add '/src' to the path of the database
+            System.out.println("Info: Database detected Development Environment");
+            path = "src/" + path;
+        }
+        path = "jdbc:sqlite:" + path;
         try {
             Class.forName("org.sqlite.JDBC");
-            return DriverManager.getConnection("jdbc:sqlite:main/java/com/data/local.db");
+            return DriverManager.getConnection(path);
         } catch (ClassNotFoundException | SQLException e) {
             System.err.println(e.getMessage());
         }
